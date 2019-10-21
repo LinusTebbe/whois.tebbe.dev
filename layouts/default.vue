75f9325cb58d97b871999bd6780d310255dbcbc6
ALIERIS-corp <linus.tebbe@alieris.dev>
2019-10-21 09:17:40 +0200

    Initial commit

---
+++ layouts/default.vue
@@ -0,0 +1,207 @@
+<!--suppress CssUnknownTarget -->
+<template>
+  <div>
+    <nav>
+      <nuxt-link to="/" class="logo">
+        <span class="big">whois.alieris.dev</span>
+        <span class="small">
+          <font-awesome-icon :icon="['fas', 'home']" />
+        </span>
+      </nuxt-link>
+      <form class="float-right" @submit.prevent="submit()">
+        <div>
+          <input
+            id="ipInput"
+            v-model.trim="ipInput"
+            type="text"
+            name="ipInput"
+            required
+            autofocus
+          />
+          <label for="ipInput">IP to query</label>
+        </div>
+      </form>
+    </nav>
+    <nuxt />
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      ipInput: ''
+    }
+  },
+  created() {
+    this.ipInput = this.$route.params.ipInput
+  },
+  methods: {
+    submit() {
+      this.$router.push('/whois/' + this.ipInput)
+    }
+  }
+}
+</script>
+
+<style>
+@import url('https://fonts.googleapis.com/css?family=Roboto&display=swap');
+
+:root {
+  --text-color: #fff;
+  --primary-color: #1e1e1e;
+  --background-color: #121212;
+  --card-background-color: #1c1c1c;
+  --card-dialog-background-color: #2e2e2e;
+  --default-padding: 14px;
+  --navbar-logo-size: 24px;
+  --light-button-padding: 4px 10px;
+  --light-button-border: 1px #fff solid;
+  --card-padding: 20px;
+  --standard-shadow: 0 2px 4px rgba(0, 0, 0, 0.5);
+  --default-border-radius: 4px;
+  --input-padding: 10px;
+  --blurry-shadow: 0 5px 5px -3px rgba(0, 0, 0, 0.2),
+    0 8px 10px 1px rgba(0, 0, 0, 0.14), 0 3px 14px 2px rgba(0, 0, 0, 0.12);
+}
+
+html {
+  min-height: 100%;
+}
+
+body,
+#__layout > div {
+  min-height: 100vh !important;
+}
+
+body {
+  font-family: 'Roboto', 'Helvetica Neue', 'Helvetica', 'Arial', sans-serif;
+  margin: 0;
+  min-height: 100%;
+  color: var(--text-color);
+  background-color: var(--background-color);
+}
+
+#ipInput {
+  width: 300px;
+  margin-top: -5px;
+}
+
+nav a,
+a:active,
+a:link {
+  color: var(--text-color);
+  text-decoration: none;
+}
+
+a:hover:not(.no-decoration) {
+  text-decoration: underline;
+  color: var(--text-color);
+}
+
+nav {
+  padding: var(--default-padding);
+  background-color: var(--primary-color);
+  -webkit-box-shadow: var(--standard-shadow);
+  -moz-box-shadow: var(--standard-shadow);
+  box-shadow: var(--standard-shadow);
+}
+
+nav .logo {
+  font-size: var(--navbar-logo-size);
+}
+
+table > tr > td {
+  padding: 4px 0;
+}
+
+table > tr > td:first-child {
+  padding-right: 10px;
+}
+
+.float-right {
+  float: right;
+}
+
+.card {
+  -webkit-box-shadow: var(--standard-shadow);
+  -moz-box-shadow: var(--standard-shadow);
+  box-shadow: var(--standard-shadow);
+  background-color: var(--card-background-color);
+  -webkit-border-radius: var(--default-border-radius);
+  -moz-border-radius: var(--default-border-radius);
+  border-radius: var(--default-border-radius);
+  padding: var(--card-padding);
+  margin: 20px;
+}
+
+.pre {
+  white-space: pre-line;
+}
+
+.margin {
+  margin: 4px;
+}
+
+.margin {
+  padding: 10px;
+}
+
+.strike-through {
+  text-decoration: line-through;
+}
+
+@media (max-width: 529px) {
+  nav .logo .big {
+    display: none;
+  }
+}
+
+@media (min-width: 530px) {
+  nav .logo .small {
+    display: none;
+  }
+}
+form:not(.inline) input {
+  display: block;
+  width: calc(100% - 20px);
+}
+
+form > div > input {
+  font-size: 14px;
+  color: var(--text-color);
+  font-family: unset;
+  border: var(--light-button-border);
+  -webkit-border-radius: var(--default-border-radius);
+  -moz-border-radius: var(--default-border-radius);
+  border-radius: var(--default-border-radius);
+  padding: var(--input-padding);
+  background: unset;
+}
+
+form {
+  background-color: inherit;
+}
+
+form > div {
+  position: relative;
+  background-color: inherit;
+}
+form > div > label {
+  top: var(--input-padding);
+  left: var(--input-padding);
+  position: absolute;
+  transition: 0.1s;
+  cursor: text;
+}
+
+form > div > input:focus + label,
+form > div > input:valid + label,
+form > div > select + label {
+  font-size: 12px;
+  top: -7px;
+  padding: 0 5px;
+  left: 5px;
+  background-color: inherit;
+}
+</style>
---
+++ pages/whois/_ipInput/index.vue
@@ -0,0 +1,166 @@
+<template>
+  <div>
+    <div v-if="response" class="card">
+      <table>
+        <tr>
+          <td>Type</td>
+          <td>IP{{ response.ipVersion }}</td>
+        </tr>
+        <tr>
+          <td>Network Range</td>
+          <td>
+            {{ response.startAddress + ' - ' + response.endAddress }}
+          </td>
+        </tr>
+        <tr v-if="response.country">
+          <td>Country</td>
+          <td>{{ response.country }}</td>
+        </tr>
+        <tr>
+          <td>NetName</td>
+          <td>{{ response.name }}</td>
+        </tr>
+        <tr v-if="response.remarks">
+          <td>Description</td>
+          <td>{{ response.remarks[0].description[0] }}</td>
+        </tr>
+        <tr>
+          <td>Address</td>
+          <td class="pre">
+            {{ findParam(vCardArray, 'adr')[1].label }}
+          </td>
+        </tr>
+        <tr>
+          <td>E-Mail</td>
+          <td>{{ findParam(vCardArray, 'email')[3] }}</td>
+        </tr>
+      </table>
+    </div>
+    <div v-else class="spinner-body">
+      <svg
+        class="spinner"
+        width="65px"
+        height="65px"
+        viewBox="0 0 66 66"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <circle
+          class="path"
+          fill="none"
+          stroke-width="6"
+          stroke-linecap="round"
+          cx="33"
+          cy="33"
+          r="30"
+        ></circle>
+      </svg>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'DetailsPage',
+  data() {
+    return {
+      response: null,
+      address: null,
+      vCardArray: null
+    }
+  },
+  async mounted() {
+    this.response = await this.fetchInformation()
+    this.vCardArray = this.getVCardArray(this.response.entities)
+  },
+  methods: {
+    fetchInformation() {
+      return new Promise((resolve) => {
+        resolve(
+          this.$axios.$get(
+            'https://rdap.db.ripe.net/ip/' + this.$route.params.ipInput
+          )
+        )
+      })
+    },
+    getVCardArray(arrayToSearchForAddress, keyToSearchFor = 'vcardArray') {
+      let returnObject = []
+      for (const prop in arrayToSearchForAddress) {
+        if (arrayToSearchForAddress.hasOwnProperty(prop)) {
+          const index = keyToSearchFor.indexOf(prop)
+          if (typeof arrayToSearchForAddress[prop] === 'object') {
+            if (index > -1) {
+              returnObject = returnObject.concat(
+                arrayToSearchForAddress[prop][1]
+              )
+            } else {
+              returnObject = this.getVCardArray(
+                arrayToSearchForAddress[prop],
+                keyToSearchFor
+              ).concat(returnObject)
+            }
+          }
+        }
+      }
+      return returnObject
+    },
+    findParam(addressArray, keyToFind) {
+      return addressArray.find((element) => element[0] === keyToFind)
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+// This is just to center the spinner
+
+.spinner-body {
+  position: absolute;
+  top: 57px;
+  bottom: 0;
+  left: 0;
+  right: 0;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+
+// Here is where the magic happens
+
+$offset: 187;
+$duration: 1.4s;
+
+.spinner {
+  animation: rotator $duration linear infinite;
+}
+
+@keyframes rotator {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(270deg);
+  }
+}
+
+.path {
+  stroke-dasharray: $offset;
+  stroke: #fff;
+  stroke-dashoffset: 0;
+  transform-origin: center;
+  animation: dash $duration ease-in-out infinite;
+}
+
+@keyframes dash {
+  0% {
+    stroke-dashoffset: $offset;
+  }
+  50% {
+    stroke-dashoffset: $offset/4;
+    transform: rotate(135deg);
+  }
+  100% {
+    stroke-dashoffset: $offset;
+    transform: rotate(450deg);
+  }
+}
+</style>
